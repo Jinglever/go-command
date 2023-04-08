752e37c38dcf4d8da5a200cb482307a4d320efb6
howard <zhihao.zhou.bupt@gmail.com>
2023-04-08 19:46:42 +0800

    feat: change AskForAnswer to AskForOption; add AskForInput

--- interact.go
+++ interact.go
@@ -7,12 +7,12 @@ import (
 	"strings"
 )
 
-// AskForAnswer asks the user a question and returns the answer.
+// AskForOption asks the user a question and returns the answer.
 // The options are presented as a list of options, and the defaultAnswer
 // is the default answer if the user just presses enter. if defaultAnswer is "",
 // then continuse ask until the user enters a valid answer.
 // The answer is returned as a string.
-func AskForAnswer(question string, options []string, defaultAnswer string) string {
+func AskForOption(question string, options []string, defaultAnswer string) string {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -27,8 +27,6 @@ func AskForAnswer(question string, options []string, defaultAnswer string) strin
 		if answer == "" {
 			if defaultAnswer != "" {
 				return defaultAnswer
-			} else {
-				fmt.Println("Please enter a valid option")
 			}
 		} else {
 			for _, option := range options {
@@ -36,7 +34,30 @@ func AskForAnswer(question string, options []string, defaultAnswer string) strin
 					return answer
 				}
 			}
-			fmt.Println("Please enter a valid option")
+		}
+	}
+}
+
+// AskForInput asks the user a question and returns the answer.
+// The answer is returned as a string.
+func AskForInput(question string, defaultAnswer string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s: ", question)
+		answer, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input: ", err)
+			continue
+		}
+
+		answer = strings.TrimSpace(answer)
+		if answer == "" {
+			if defaultAnswer != "" {
+				return defaultAnswer
+			}
+		} else {
+			return answer
 		}
 	}
 }
--- test/main.go
+++ test/main.go
@@ -8,6 +8,15 @@ import (
 
 func main() {
 	options := []string{"a", "b", "c"}
-	answer := jgcmd.AskForAnswer("请选择一个选项", options, "a")
+	answer := jgcmd.AskForOption("请选择一个选项", options, "a")
 	fmt.Printf("你选择了：%s\n", answer)
+
+	answer = jgcmd.AskForOption("请选择一个选项", options, "")
+	fmt.Printf("你选择了：%s\n", answer)
+
+	answer = jgcmd.AskForInput("请输入一个字符串", "default")
+	fmt.Printf("你输入了：%s\n", answer)
+
+	answer = jgcmd.AskForInput("请输入一个字符串", "")
+	fmt.Printf("你输入了：%s\n", answer)
 }
